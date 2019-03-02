64a56b95d3018468ce7c0e115c76e9c10a80071a
吴勇胜（Vincent） <1025362683@qq.com>
2019-03-02 22:11:01 +0800

    Update Test3.java

--- src/test/java/wysh/java_selenium_best/Test3.java
+++ src/test/java/wysh/java_selenium_best/Test3.java
@@ -7,7 +7,7 @@ public class Test3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          WebDriver driver = new ChromeDriver();
-         driver.get("http://wysh.site");
+         driver.get("http://baidu.com");
          try {
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
