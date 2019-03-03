0f7630a2af8b7c3dde675fc551a7c6cdcf47f2e4
wuyongsheng <1025362683@qq.com>
2019-03-03 15:51:09 +0800

    test delete

---
+++ src/test/java/wysh/java_selenium_best/NewTest.java
@@ -0,0 +1,19 @@
+package wysh.java_selenium_best;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
