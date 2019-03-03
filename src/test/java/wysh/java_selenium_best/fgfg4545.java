cd57a4dbca25cd90a7a9bee2f8aece692cbf4bd5
wuyongsheng <1025362683@qq.com>
2019-03-03 17:48:01 +0800

    # WARNING: head commit changed in the meantime
    7777
    
    Change-Id: Ia212a884d2eaaa022bdc9cc8f862d31c2540031c
    Signed-off-by: wuyongsheng <1025362683@qq.com>

---
+++ src/test/java/wysh/java_selenium_best/fgfg4545.java
@@ -0,0 +1,14 @@
+package wysh.java_selenium_best;
+
+public class fgfg4545 {
+
+	public fgfg4545() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
