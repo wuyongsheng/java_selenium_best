f77c0355406c0b378aecd8ee814ccf951a382139
wuyongsheng <1025362683@qq.com>
2019-03-03 17:49:10 +0800

    cc

--- src/test/java/wysh/java_selenium_best/drfe34.java
+++ src/test/java/wysh/java_selenium_best/drfe34.java
@@ -1,10 +1,6 @@
 package wysh.java_selenium_best;
 
-public class fgfg4545 {
-
-	public fgfg4545() {
-		// TODO Auto-generated constructor stub
-	}
+public class drfe34 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
